3cac8e421142c0b2a22f48852637815f659a209a
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-30 19:14:26 -0300

    Add github battle again

---
+++ github-battle/app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Routes = require('./config/routes');
+
+ReactDOM.render(
+    Routes,
+    document.getElementById('app')
+);
