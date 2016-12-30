9fbab2a1ba56053698d484d0396c9e1466e2c5fd
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-30 19:03:52 -0300

    Remove github-battle and add index

--- github-battle/app/index.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Routes = require('./config/routes');
-
-ReactDOM.render(
-    Routes,
-    document.getElementById('app')
-);
